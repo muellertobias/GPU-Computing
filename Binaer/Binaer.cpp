1180653fc2fd1edaf027ef8166591ebd08c29582
J. Kirsch <htw.jukirsch@gmail.com>
2017-10-25 16:12:04 +0200

    Binaer hinzugefügt

---
+++ Binaer/Binaer.cpp
@@ -0,0 +1,79 @@
+
+#include <iostream> 
+#include <stdlib.h>
+#include <ctime>
+#include <math.h>
+using namespace std;
+
+
+void random_array(int *a, int size)
+{
+	cout << "-1 init" << endl;
+	for (int i = 0; i < size; i++) {
+		a[i] = -1;
+	}
+	cout << "random init" << endl;
+	int j;
+	int temp;
+	int remain = size;
+	//clock_t last = clock();
+	srand(time(0));
+	for (int i = 0; i < size;i++) {
+		
+		temp = rand();
+		j = 0;
+		while (a[j] != -1 && a[j] != temp)
+		{
+			j++;
+		}
+		if (a[j] == -1) {
+			a[j] = temp;
+			cout  << remain - i << "	Plaetze warten auf Wert 	"<< i << "	Plaetze besitzen Wert" << endl;
+		}
+		else
+		{
+			i--;
+		}
+
+	}
+}
+int vgl(const void *x,  const void *y) { // Die Funktion macht den Abfuck noch größer
+	return *(int*)x - *(int*)y;
+}
+
+int main()
+{
+	
+	const int size = 268435456;
+	int *a = new int[size];
+	random_array(a, size);
+	qsort(a, sizeof(a)/sizeof(int), sizeof(int), vgl); // Sortieren ... kp das Teil ist ziemlicher abfuck :-D ... Prüfung
+	for (int i = 0; i < size;i++) {
+		cout << a[i] << endl;
+	} 
+	int tmp = 0;
+	cin >> tmp;
+}
+
+/*int binary_search(const int suchwert, int *a, const int eintraege)
+{
+	const int NICHT_GEFUNDEN = -1;
+	int links = 0;                     
+	int rechts = eintraege - 1;         
+
+	while (links <= rechts)
+	{
+		int mitte = links + ((rechts - links) / 2); 
+
+		if (M[mitte] == suchwert)      
+			return mitte;
+		else
+			if (M[mitte] > suchwert)
+				rechts = mitte - 1;     
+			else 
+				links = mitte + 1;      
+	}
+
+	return NICHT_GEFUNDEN;
+	// alternativ: return -mitte ; // gute Positionsschaetzung: "nach (-Returnwert) wuerde es hingehoeren"
+}*/
