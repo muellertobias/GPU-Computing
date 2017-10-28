a8a106b8bc770a2aa9e978507cff7a4fde3bb269
Tobias Müller <muellertobias789@gmail.com>
2017-10-28 17:28:03 +0200

    aufgabe a + b

--- Binaer/Binaer.cpp
+++ Binaer/Binaer.cpp
@@ -58,6 +58,8 @@ void init_random_array(unsigned long *a, unsigned long *s, long size, long size_
 
 int main()
 {
+	clock_t all = clock();
+
 	const long size = 268435456;
 	const long size_s = size / 2;
 	unsigned long *a = new unsigned long[size];
@@ -78,9 +80,52 @@ int main()
 			counter++;
 		}
 	}
-
+	cout << "Runtime without OpenMP: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
 	cout << "Found: " << counter << endl;
-	cout << "Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
+	counter = 0;
+	start = clock();
+
+	#pragma omp parallel for 
+	for (int i = 0; i < size_s; i++)
+	{
+		long index = binary_search(s[i], a, size);
+		if (index != -1)
+		{
+			counter++;
+		}
+	}
+	cout << "Runtime with static scheduling: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
+	cout << "Found: " << counter << endl;
+	counter = 0;
+	start = clock();
+
+	#pragma omp parallel for schedule(dynamic)
+	for (int i = 0; i < size_s; i++)
+	{
+		long index = binary_search(s[i], a, size);
+		if (index != -1)
+		{
+			counter++;
+		}
+	}
+	cout << "Runtime  with dynamic scheduling: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
+	cout << "Found: " << counter << endl;
+	counter = 0;
+	start = clock();
+
+	#pragma omp parallel for schedule(guided[1])
+	for (int i = 0; i < size_s; i++)
+	{
+		long index = binary_search(s[i], a, size);
+		if (index != -1)
+		{
+			counter++;
+		}
+	}
+	cout << "Runtime  with static guided [c=1]: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
+	cout << "Found: " << counter << endl;
+
+	cout << "Runtime complete: " << (double)(clock() - all) / CLOCKS_PER_SEC << endl;
 	cout << "Press any key..." << endl;
 	int tmp = 0;
 	cin >> tmp;
