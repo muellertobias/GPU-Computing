9465c8d2f0cf77b9592f4bbf9a5dfd321ba19a2e
Tobias Müller <muellertobias789@gmail.com>
2017-10-28 16:49:01 +0200

    aufgabenteil a

--- Binaer/Binaer.cpp
+++ Binaer/Binaer.cpp
@@ -7,7 +7,30 @@
 
 using namespace std;
 
-void init_random_array(unsigned long *a, unsigned long *s, unsigned long size, unsigned long size_s)
+long binary_search(const unsigned long suchwert, unsigned long *a, const  long eintraege)
+{
+	const long NICHT_GEFUNDEN = -1;
+	long links = 0;
+	long rechts = eintraege - 1;
+
+	while (links <= rechts)
+	{
+		long mitte = links + ((rechts - links) / 2);
+
+		if (a[mitte] == suchwert)
+			return mitte;
+		else
+			if (a[mitte] > suchwert)
+				rechts = mitte - 1;
+			else
+				links = mitte + 1;
+	}
+
+	return NICHT_GEFUNDEN;
+	// alternativ: return -mitte ; // gute Positionsschaetzung: "nach (-Returnwert) wuerde es hingehoeren"
+}
+
+void init_random_array(unsigned long *a, unsigned long *s, long size, long size_s)
 {
 	cout << "init: array a" << endl;
 	
@@ -15,7 +38,7 @@ void init_random_array(unsigned long *a, unsigned long *s, unsigned long size, u
 
 	a[0] = (1 + rand() % 3);
 	// parallelisierung nicht möglich 
-	for (unsigned long i = 1; i < size; i++)
+	for (long i = 1; i < size; i++)
 	{
 		a[i] = a[i - 1] + (1 + rand() % 100);
 	}
@@ -35,42 +58,28 @@ void init_random_array(unsigned long *a, unsigned long *s, unsigned long size, u
 
 int main()
 {
-	const unsigned long size = 268435456;
-	const unsigned long size_s = 10000;
+	const long size = 268435456;
+	const long size_s = 10000;
 	unsigned long *a = new unsigned long[size];
 	unsigned long *s = new unsigned long[size_s];
 
 	init_random_array(a, s, size, size_s);
-
+	int counter = 0;
 	clock_t start = clock();
 
-	// do it!
+	//#pragma omp parallel for
+	for (int i = 0; i < size_s; i++)
+	{
+		long index = binary_search(s[i], a, size);
+		if (index != -1)
+		{
+			counter++;
+		}
+	}
 
-	cout << "Laufzeit: " << (clock() - start) / CLOCKS_PER_SEC << endl;
+	cout << "Found: " << counter << endl;
+	cout << "Runtime: " << (clock() - start) / CLOCKS_PER_SEC << endl;
 	cout << "Press any key..." << endl;
 	int tmp = 0;
 	cin >> tmp;
 }
-
-/*int binary_search(const int suchwert, int *a, const int eintraege)
-{
-	const int NICHT_GEFUNDEN = -1;
-	int links = 0;                     
-	int rechts = eintraege - 1;         
-
-	while (links <= rechts)
-	{
-		int mitte = links + ((rechts - links) / 2); 
-
-		if (M[mitte] == suchwert)      
-			return mitte;
-		else
-			if (M[mitte] > suchwert)
-				rechts = mitte - 1;     
-			else 
-				links = mitte + 1;      
-	}
-
-	return NICHT_GEFUNDEN;
-	// alternativ: return -mitte ; // gute Positionsschaetzung: "nach (-Returnwert) wuerde es hingehoeren"
-}*/
