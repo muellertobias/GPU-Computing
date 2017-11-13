b1726a3fce7f64ca82ddbce85d6bdb27a94c96a2
Tobias Müller <muellertobias789@gmail.com>
2017-11-13 08:58:07 +0100

    Added function to read a kernel source file

--- VectorMulti/VectorAdd.cpp
+++ VectorMulti/VectorAdd.cpp
@@ -13,24 +13,28 @@
 // Kopiert von https://www.olcf.ornl.gov/tutorials/opencl-vector-addition/
 
 // OpenCL kernel. Each work item takes care of one element of c
-const char *kernelSource = "\n" \
-"#pragma OPENCL EXTENSION cl_khr_fp64 : enable                    \n" \
-"__kernel void vecAdd(  __global double *a,                       \n" \
-"                       __global double *b,                       \n" \
-"                       __global double *c,                       \n" \
-"                       const unsigned int n)                    \n" \
-"{                                                               \n" \
-"    //Get our global thread ID                                  \n" \
-"    int id = get_global_id(0);                                  \n" \
-"                                                                \n" \
-"    //Make sure we do not go out of bounds                      \n" \
-"    if (id < n)                                                 \n" \
-"        c[id] = a[id] + b[id];                                  \n" \
-"}                                                               \n" \
-"\n";
+//const char *kernelSource = "\n" \
+//"#pragma OPENCL EXTENSION cl_khr_fp64 : enable                    \n" \
+//"__kernel void vecAdd(  __global double *a,                       \n" \
+//"                       __global double *b,                       \n" \
+//"                       __global double *c,                       \n" \
+//"                       const unsigned int n)                    \n" \
+//"{                                                               \n" \
+//"    //Get our global thread ID                                  \n" \
+//"    int id = get_global_id(0);                                  \n" \
+//"                                                                \n" \
+//"    //Make sure we do not go out of bounds                      \n" \
+//"    if (id < n)                                                 \n" \
+//"        c[id] = a[id] + b[id];                                  \n" \
+//"}                                                               \n" \
+//"\n";
+
+char* readSourceFile(const char* filename);
 
 int main(int argc, char* argv[])
 {
+	const char* kernelSource = readSourceFile("Kernel.cl");
+
 	// Length of vectors
 	unsigned int n = 100000;
 
@@ -53,8 +57,8 @@ int main(int argc, char* argv[])
 	cl_program program;				  // program
 	cl_kernel kernel;				  // kernel
 
-									  // Size, in bytes, of each vector
-	size_t bytes = n * sizeof(double);
+									 
+	size_t bytes = n * sizeof(double);  // Size, in bytes, of each vector
 
 	// Allocate memory for each vector on host
 	h_a = (double*)malloc(bytes);
@@ -62,11 +66,10 @@ int main(int argc, char* argv[])
 	h_c = (double*)malloc(bytes);
 
 	// Initialize vectors on host
-	int i;
-	for (i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		h_a[i] = sinf(i)*sinf(i);
-		h_b[i] = cosf(i)*cosf(i);
+		h_a[i] = sinf(i) * sinf(i);
+		h_b[i] = cosf(i) * cosf(i);
 	}
 
 	size_t globalSize, localSize;
@@ -138,11 +141,9 @@ int main(int argc, char* argv[])
 
 	//Sum up vector c and print result divided by n, this should equal 1 within error
 	double sum = 0;
-	for (i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		sum += h_c[i];
-		/*printf("h_a[%d] = %f\n", i, h_a[i]);
-		printf("h_b[%d] = %f\n", i, h_b[i]);
-		printf("h_c[%d] = %f\n", i, h_c[i]);*/
 	}
 	printf("final result: %f\n", sum / n);
 
@@ -160,7 +161,24 @@ int main(int argc, char* argv[])
 	free(h_b);
 	free(h_c);
 
+	printf("Press any key and then press enter...");
 	int a = 0;
 	std::cin >> a;
 	return 0;
+}
+
+char* readSourceFile(const char* filename) 
+{
+	FILE *fp;
+	fopen_s(&fp, filename, "rb");
+	fseek(fp, 0, SEEK_END);
+	long size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	char* source = (char*)malloc(size + 1);
+	fread(source, size, 1, fp);
+	fclose(fp);
+	source[size] = 0;
+	
+	return source;
 }
