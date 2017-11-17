31791ff4f30f66a309e10ba59ce7372cf9102054
Tobias Müller <muellertobias789@gmail.com>
2017-11-17 21:28:08 +0100

    recreated kernel; structure; improved maxtrixVectorMul; created function for magnitude of a vector;

--- OpenCLMatrix/Kernel.cl
+++ OpenCLMatrix/Kernel.cl
@@ -1,15 +1,16 @@
 #pragma OPENCL EXTENSION cl_khr_fp64 : enable
-__kernel void OpenCLMatrix(__global double *a, __global double *b, __global double *c, __global double *temp, const unsigned int n)
+__kernel void OpenCLMatrix(__global double *A, __global double *b, __global double *c, const unsigned int n)
 {
     //Get our global thread ID
     int id = get_global_id(0);
 	
     //Make sure we do not go out of bounds
-	unsigned int maxSize = n*n;
-	unsigned int posB = 0;
-	if (id < maxSize)
+	if (id < n)
 	{
-		posB = id % n;
-		temp[id] = A[id] * b[posB];
+		c[id] = 0;
+		for (int i = 0; i < n; i++) 
+		{
+			c[id] += A[id * n + i] * b[i];
+		}
 	}
 }  
--- OpenCLMatrix/OpenCLMatrix.cpp
+++ OpenCLMatrix/OpenCLMatrix.cpp
@@ -4,20 +4,225 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <iostream>
-#include <ctime>
+#include <time.h>
 #include <string>
 #include <sstream>
 
 
-
-// Kopiert von https://www.olcf.ornl.gov/tutorials/opencl-vector-addition/
-
 char* readSourceFile(const char* filename);
 int readBool(const char c);
 void printMatrix(double* matrix, int n, int m);
+void initVector(double* vector, int n);
+void initVectorWithNull(double* vector, int n);
+void initMatrix(double* matrix, int n, int m);
+void initMatrixWithNull(double* matrix, int n, int m);
+
+void matrixVectorMultiplication(double* A, double* b, double* result, int n);
+double magnitudeVector(double* vector, int n);
 
 using namespace std;
+
+int main(int argc, char* argv[])
+{
+	const char* kernelSource = readSourceFile("Kernel.cl");
+
+	// Host input vectors
+	double *h_A;
+	double *h_b;
+
+	// Host output vector
+	double *h_c;
+
+	// Length of vectors
+	unsigned int n = 200;
+
+
+	// Device input buffers
+	cl_mem d_A;
+	cl_mem d_b;
+	// Device output buffer
+	cl_mem d_c;
+
+
+	cl_platform_id cpPlatform;		  // OpenCL platform
+	cl_device_id device_id;           // device ID
+	cl_context context;				  // context
+	cl_command_queue queue;			  // command queue
+	cl_program program;				  // program
+	cl_kernel kernel;				  // kernel
+
+
+	size_t bytes = n * sizeof(double);  // Size, in bytes, of each vector
+
+	// Allocate memory for each vector on host
+	h_A = (double*)malloc(bytes * bytes);
+	h_b = (double*)malloc(bytes);
+	h_c = (double*)malloc(bytes);
+
+	// Initialize vectors on host
+	printf_s("init...\n");
+	initVector(h_b, n);
+	initVectorWithNull(h_c, n);
+	initMatrix(h_A, n, n);
+
+	printf_s("Press 'y' to show matrix or press any key ... ");
+	int showMatrix = readBool('y');
+	if (showMatrix) 
+	{
+		printMatrix(h_A, n, n);
+		printf_s("\n");
+		printMatrix(h_b, 1, n);
+	}
+
+	clock_t start_normal = clock();
+
+	matrixVectorMultiplication(h_A, h_b, h_c, n);
+
+	clock_t stop_normal = clock();
+	clock_t difference = stop_normal - start_normal;
+	double t = (double)difference / CLOCKS_PER_SEC;
+
+	printf_s("Finished Normal: %f s\n", t);
+	// ---------------------------------------- Bis hier
+	double sum = magnitudeVector(h_c, n);
+	printf_s("final result: %f\n", sum / n);
+
+	if (showMatrix) 
+	{
+		printf_s("Ergebnis:\n");
+		printMatrix(h_c, 1, n);
+	}
+	
+	initVectorWithNull(h_c, n);
+
+	size_t globalSize, localSize;
+	cl_int err;
+
+	// Number of work items in each local work group
+	localSize = 64;
+
+	// Number of total work items - localSize must be devisor
+	globalSize = ceil(n / (float)localSize) * localSize;
+
+	// Bind to platform
+	err = clGetPlatformIDs(1, &cpPlatform, NULL);
+	printf("GetPlatfrom: %d\n", err);
+
+	// Get ID for the device
+	err = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
+	printf("GetDeviceIDs: %d\n", err);
+
+	// Create a context  
+	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+	printf("CreateContext: %d\n", err);
+
+	// Create a command queue 
+	//queue = clCreateCommandQueue(context, device_id, NULL, &err);
+	queue = clCreateCommandQueue(context, device_id, NULL, &err);
+	printf("CreateCommandQueue: %d\n", err);
+
+	// Create the compute program from the source buffer
+	program = clCreateProgramWithSource(context, 1, (const char **)& kernelSource, NULL, &err);
+	printf("CreateProgramWithSource: %d\n", err);
+
+	// Build the program executable 
+	clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+
+	// Create the compute kernel in the program we wish to run
+	kernel = clCreateKernel(program, "OpenCLMatrix", &err);
+	printf("CreateKernel: %d\n", err); 
+
+	// Create the input and output arrays in device memory for our calculation
+	d_A = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes * bytes, NULL, NULL);
+	d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
+	d_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
+
+	// Write our data set into the input array in device memory
+	err = clEnqueueWriteBuffer(queue, d_A, CL_TRUE, 0, bytes * bytes, h_A, 0, NULL, NULL);
+	err |= clEnqueueWriteBuffer(queue, d_b, CL_TRUE, 0, bytes, h_b, 0, NULL, NULL);
+	printf("EnqueueWriteBuffers: %d\n", err);
+
+	// Set the arguments to our compute kernel
+	err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_A);
+	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+	err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+	err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &n);
+	printf("SetKernelArgs: %d\n", err);
+
+	// -------------------------------------------------------------------------------------- Start auf GPU
+	clock_t start_GPU = clock();
+	// Execute the kernel over the entire range of the data set  
+	err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
+
+	// Wait for the command queue to get serviced before reading back results
+	clFinish(queue);
+
+	// Read the results from the device
+	clEnqueueReadBuffer(queue, d_c, CL_TRUE, 0, bytes, h_c, 0, NULL, NULL);
+
+	clock_t stop_GPU = clock();
+	difference = stop_GPU - start_GPU;
+	t = (double)difference / CLOCKS_PER_SEC;
+	printf_s("Finished GPU: %f s\n", t);
+
+
+	//Sum up vector c and print result divided by n, this should equal 1 within error
+	sum = magnitudeVector(h_c, n);
+	printf_s("final result: %f\n", sum / n);
+	// -------------------------------------------------------------------------------------- Ende GPU
+	
+	// release OpenCL resources
+	clReleaseMemObject(d_A);
+	clReleaseMemObject(d_b);
+	clReleaseMemObject(d_c);
+	clReleaseProgram(program);
+	clReleaseKernel(kernel);
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
+
+	//release host memory
+	free(h_A);
+	free(h_b);
+	free(h_c); 
+
+	printf("Press any key and then press enter...");
+	return getchar();
+}
+
+int readBool(const char c) 
+{
+	char input = 0;
+	while ((input = getchar()) != '\n');
+	return input == c;
+}
+
+void printMatrix(double* matrix, int n, int m) 
+{
+	for (int n0 = 0; n0 < n; n0++)
+	{
+		for (int m0 = 0; m0 < m; m0++)
+		{
+			printf_s("%f ", matrix[n0 * n + m0]);
+		}
+		printf_s("\n");
+	}
+}
+
+char* readSourceFile(const char* filename)
+{
+	FILE *fp;
+	fopen_s(&fp, filename, "rb");
+	fseek(fp, 0, SEEK_END);
+	long size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	char* source = (char*)malloc(size + 1);
+	fread(source, size, 1, fp);
+	fclose(fp);
+	source[size] = 0;
+
+	return source;
+}
 
 void initVector(double* vector, int n)
 {
@@ -58,237 +263,42 @@ void initMatrixWithNull(double* matrix, int n, int m)
 	}
 }
 
-int main(int argc, char* argv[])
+void matrixVectorMultiplication(double* A, double* b, double* result, int n) 
 {
-	const char* kernelSource = readSourceFile("Kernel.cl");
-
-	// Host input vectors
-	double *h_A;
-	double *h_b;
-	// temp Vector
-	double *temp;
-	// Host output vector
-	double *h_c;
-
-	// Length of vectors
-	unsigned int n = 200;
-
-
-	// Device input buffers
-	cl_mem d_A;
-	cl_mem d_b;
-	// Device output buffer
-	cl_mem d_c;
-	// temp
-	cl_mem d_temp;
-
-	cl_platform_id cpPlatform;		  // OpenCL platform
-	cl_device_id device_id;           // device ID
-	cl_context context;				  // context
-	cl_command_queue queue;			  // command queue
-	cl_program program;				  // program
-	cl_kernel kernel;				  // kernel
-
-
-	size_t bytes = n * sizeof(double);  // Size, in bytes, of each vector
-
-										// Allocate memory for each vector on host
-	h_A = (double*)malloc(bytes*bytes);
-	h_b = (double*)malloc(bytes);
-	h_c = (double*)malloc(bytes);
-	temp = (double*)malloc(bytes*bytes);
-
-	// Initialize vectors on host
-	printf_s("init...\n");
-	initVector(h_b, n);
-	initVectorWithNull(h_c, n);
-	initMatrixWithNull(temp, n, n);
-	initMatrix(h_A, n, n);
-
-	printf_s("Press 'y' to show matrix or press any key ... ");
-	int showMatrix = readBool('y');
-	if (showMatrix) {
-		printMatrix(h_A, n, n);
-		printf_s("\n");
-		printMatrix(h_b, 1, n);
-	}
-
-	unsigned int posB;
+	/*unsigned int posB;
 	int rowCounter = 0;
 	int vectorRowCurser = 0;
 	int rowAdd = 0;
-	// ---------------------------------------- Normale Berechnung ohne GPU
-	clock_t start_normal = clock();
-	for (int i = 0; i < n*n; i++)
+
+	for (int i = 0; i < n * n; i++)
 	{
 		posB = (i % n);
-		rowAdd += h_A[i] * h_b[posB];
+		rowAdd += A[i] * b[posB];
 		rowCounter++;
 		if (rowCounter == n) {
-			h_c[vectorRowCurser] = rowAdd;
+			result[vectorRowCurser] = rowAdd;
 			rowAdd = 0;
 			vectorRowCurser++;
 			rowCounter = 0;
 		}
-	}
+	}*/
 
-	clock_t stop_normal = clock();
-	clock_t difference = stop_normal - start_normal;
-	double t = (double)difference / CLOCKS_PER_SEC;
-	cout << "Finished Normal: " << t << " s" << endl;
-	// ---------------------------------------- Bis hier
-	if (showMatrix) {
-		cout << "\n Ergebnis \n" << endl;
-		for (int i = 0; i < n; i++) {
-			cout << h_c[i] << endl;
+	for (int i = 0; i < n; i++) 
+	{
+		result[i] = 0;
+		for (int j = 0; j < n; j++) 
+		{
+			result[i] += A[i * n + j] * b[j];
 		}
 	}
-	
-	initVectorWithNull(h_c, n);
+}
 
-	size_t globalSize, localSize;
-	cl_int err;
-
-	// Number of work items in each local work group
-	localSize = 64;
-
-	// Number of total work items - localSize must be devisor
-	globalSize = ceil(n / (float)localSize)*localSize;
-	globalSize *= globalSize;
-
-	// Bind to platform
-	err = clGetPlatformIDs(1, &cpPlatform, NULL);
-	printf("GetPlatfrom: %d\n", err);
-
-	// Get ID for the device
-	err = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-	printf("GetDeviceIDs: %d\n", err);
-
-	// Create a context  
-	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-	printf("CreateContext: %d\n", err);
-
-	// Create a command queue 
-	//queue = clCreateCommandQueue(context, device_id, NULL, &err);
-	queue = clCreateCommandQueue(context, device_id, NULL, &err);
-	printf("CreateCommandQueue: %d\n", err);
-
-	// Create the compute program from the source buffer
-	program = clCreateProgramWithSource(context, 1,
-		(const char **)& kernelSource, NULL, &err);
-	printf("CreateProgramWithSource: %d\n", err);
-
-	// Build the program executable 
-	clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-
-	// Create the compute kernel in the program we wish to run
-	kernel = clCreateKernel(program, "OpenCLMatrix", &err);
-	printf("CreateKernel: %d\n", err);
-
-	// Create the input and output arrays in device memory for our calculation
-	d_A = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
-	d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
-	d_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
-	d_temp = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
-
-	// Write our data set into the input array in device memory
-	err = clEnqueueWriteBuffer(queue, d_A, CL_TRUE, 0,
-		bytes, h_A, 0, NULL, NULL);
-	err |= clEnqueueWriteBuffer(queue, d_b, CL_TRUE, 0,
-		bytes, h_b, 0, NULL, NULL);
-	printf("EnqueueWriteBuffers: %d\n", err);
-
-	// Set the arguments to our compute kernel
-	err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_A);
-	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
-	err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
-	err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &d_temp);
-	err |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &n);
-	printf("SetKernelArgs: %d\n", err);
-
-	// -------------------------------------------------------------------------------------- Start auf GPU
-	clock_t start_GPU = clock();
-	// Execute the kernel over the entire range of the data set  
-	err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize,
-		0, NULL, NULL);
-
-	// Wait for the command queue to get serviced before reading back results
-	clFinish(queue);
-
-	// Read the results from the device
-	clEnqueueReadBuffer(queue, d_temp, CL_TRUE, 0,
-		bytes, temp, 0, NULL, NULL);
-
-	//Sum up vector c and print result divided by n, this should equal 1 within error
+double magnitudeVector(double* vector, int n)
+{
 	double sum = 0;
-	int row = n;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < row;j++)
-		{
-			h_c[i] += temp[i];
-		}
-		row += n;
-		//cout << h_c[i] << endl;
+		sum += vector[i];
 	}
-	clock_t stop_GPU = clock();
-	difference = stop_GPU - start_GPU;
-	t = (double)difference / CLOCKS_PER_SEC;
-	cout << " \n Finished GPU: " << t << " s" << endl;
-	// -------------------------------------------------------------------------------------- Ende GPU
-	//printf("final result: %f\n", sum / n);
-
-	// release OpenCL resources
-	clReleaseMemObject(d_A);
-	clReleaseMemObject(d_b);
-	clReleaseMemObject(d_c);
-	clReleaseMemObject(d_temp);
-	clReleaseProgram(program);
-	clReleaseKernel(kernel);
-	clReleaseCommandQueue(queue);
-	clReleaseContext(context);
-
-	//release host memory
-	free(h_A);
-	free(h_b);
-	free(h_c);
-	free(temp); 
-
-	printf("Press any key and then press enter...");
-	return getchar();
-}
-
-int readBool(const char c) 
-{
-	char input = 0;
-	while ((input = getchar()) != '\n');
-	return input == c;
-}
-
-void printMatrix(double* matrix, int n, int m) 
-{
-	for (int n0 = 0; n0 < n; n0++)
-	{
-		for (int m0 = 0; m0 < m; m0++)
-		{
-			printf_s("%f ", matrix[n0 * n + m0]);
-		}
-		printf_s("\n");
-	}
-}
-
-char* readSourceFile(const char* filename)
-{
-	FILE *fp;
-	fopen_s(&fp, filename, "rb");
-	fseek(fp, 0, SEEK_END);
-	long size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-
-	char* source = (char*)malloc(size + 1);
-	fread(source, size, 1, fp);
-	fclose(fp);
-	source[size] = 0;
-
-	return source;
+	return sum;
 }
