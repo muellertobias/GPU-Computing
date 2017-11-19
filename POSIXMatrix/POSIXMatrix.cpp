c0afa5179cc8eaf1845464e1b93ffd3e6f8a91de
Tobias Müller <muellertobias789@gmail.com>
2017-11-19 16:34:58 +0100

    refactored

--- POSIXMatrix/POSIXMatrix.cpp
+++ POSIXMatrix/POSIXMatrix.cpp
@@ -6,15 +6,30 @@
 
 #define _UWIN
 
-const int P = 16;
+const int P = 4;
 const int N = 160;
-const int rows = (N / P);
-float **A = new float*[N];
-float b[N];
-float c[N];
-typedef struct par { int base; long long sum; float sumRows[rows]; } ThreadParameters;
+
+typedef struct
+{
+	int id;
+	float** matrixA;
+	float* vectorB;
+	float* vectorC;
+	long nValues;
+	int nThreads;
+} ThreadParameters;
+
 using namespace std;
 
+double magnitudeVector(float* vector, const unsigned int n)
+{
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += vector[i];
+	}
+	return sum;
+}
 
 void initVectorWithRandom(float* vector, int n)
 {
@@ -47,22 +62,33 @@ void initMatrixWithRandom(float** matrix, int n, int m)
 	}
 }
 
-void* threaded_add(void* vp) {
-	ThreadParameters* p = (ThreadParameters*)vp;
-	for (int i = p->base; i < p->base + N / P; i++) 
+void* threaded_add(void* parameters)
+{
+	ThreadParameters* param = (ThreadParameters*)parameters;
+
+	float** A = param->matrixA;
+	float* c = param->vectorC;
+	float* b = param->vectorB;
+
+	int begin = param->id * (int)((double)param->nValues / (double)param->nThreads);
+	int end = begin + (int)((double)param->nValues / (double)param->nThreads);
+
+	for (unsigned long i = begin; i < end; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
 			c[i] += A[i][j] * b[j];
 		}
 	}
-	return p;
+
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	pthread_t thr[P];
-	ThreadParameters param[P];
+	float **A = new float*[N];
+	float b[N];
+	float c[N];
 
 	cout << "Init..." << endl;
 	initVectorWithRandom(b, N);
@@ -73,25 +99,34 @@ int main(int argc, char *argv[])
 	//-----Sequenz-----
 	clock_t startSeq = clock();
 
-	float cSeq[N];
-	initVectorWithNull(cSeq, N);
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++)
 		{
-			cSeq[i] += A[i][j] * b[j];
+			c[i] += A[i][j] * b[j];
 		}
 	}
 
 	double endSeq = (double)(clock() - startSeq) / CLOCKS_PER_SEC;
-	cout << "Time with sequenz: " << endSeq << endl;
 
+	cout << "Time with sequenz: " << endSeq << endl;
+	cout << "|c| = " << magnitudeVector(c, N) << endl;
+	initVectorWithNull(c, N);
 	cout << "4 Threads:  Calculate A x b = c ..." << endl;
+
 	//-----Thread-----
+	pthread_t thr[P];
+	ThreadParameters param[P];
+	pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 	clock_t startThread = clock();
 	for (int i = 0; i < P; i++)
 	{
-		param[i].sum = 0;
-		param[i].base = i * (N / P);
+		param[i].id = i;
+		param[i].nThreads = P;
+		param[i].nValues = N;
+		param[i].matrixA = A;
+		param[i].vectorB = b;
+		param[i].vectorC = c;
 		pthread_create(&thr[i], NULL, threaded_add, (void*)&param[i]);
 	}
 
@@ -101,8 +136,8 @@ int main(int argc, char *argv[])
 	}
 
 	double endThread = (double)(clock() - startThread) / CLOCKS_PER_SEC;
-	cout << "Time with threads: " << endThread;
-
+	cout << "Time with threads: " << endThread << endl;
+	cout << "|c| = " << magnitudeVector(c, N) << endl;
 	getchar();
 	return 0;
 }
